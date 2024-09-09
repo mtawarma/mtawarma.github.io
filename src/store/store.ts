6e1d3715fa586210ba3e0cea0be74a2fe692fdf7
Tawarmalani, Mohit <mtawarma@purdue.edu>
2024-09-08 20:24:36 -0400

    update

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,5 +2,42 @@ import { persistentAtom } from '@nanostores/persistent';
 
 export const $page = persistentAtom("0");
 
+export const homeelem = document.querySelector('.pagehome');
+export const resumeelem = document.querySelector('.pageresume');
+export const bookelem = document.querySelector('.pagebook');
+export const pubelem = document.querySelector('.pagepub');
+export const confelem = document.querySelector('.pageconf');
+export const honorelem = document.querySelector('.pagehonor');
+const myMap = new Map([
+   ['home', homeelem],
+   ['resume', resumeelem],
+   ['book', bookelem],
+   ['publications', pubelem],
+   ['conferences', confelem],
+   ['honors', honorelem]
+]);
+export const pages: string[] = ["home", "resume", "book", "publications", "conferences", "honors"];
+
+$page.subscribe((page) => {
+  for (let i=1; i<7; i++) {
+     let mappage = myMap.get(pages[i-1]!);
+     if (i == parseInt(page!)) {
+       if (mappage!.classList.contains('navlink')) {
+          console.log("3. removing navlink from ", i);
+          mappage!.classList.remove('navlink');
+       }
+       console.log("3. adding myactive to ", i);
+       mappage!.classList.add('myactive');
+     } else {
+       if (mappage!.classList.contains('myactive')) {
+          console.log("1. removing myactive from ", i);
+          mappage!.classList.remove('myactive');
+       }
+       console.log("4. adding navlink to ", i);
+       mappage!.classList.add('navlink');
+     }
+  }
+})
+
 
 
