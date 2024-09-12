dd658f6a83a3ea2641db7327ab29955a02d7f63e
Tawarmalani, Mohit <mtawarma@purdue.edu>
2024-09-12 10:49:46 -0400

    add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mohit.prof/',
-  integrations: [svelte(), tailwind({applyBaseStyles: false,})]
+  integrations: [svelte(), tailwind({
+    applyBaseStyles: false
+  }), 
+  sitemap()]
 });
