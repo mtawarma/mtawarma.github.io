9a0d6a2e09e2d52ce156e177fe64dd55616d4c6f
Tawarmalani, Mohit <mtawarma@purdue.edu>
2024-09-25 17:45:38 -0400

    fix font in drawer side menu

---
+++ additional/tmp.js
@@ -0,0 +1,22 @@
+document.querySelector('html').setAttribute('data-theme', theme);
+<script>
+  // figure out user's preferred theme and set it as html class for tailwind before paint
+  (function () {
+    if (typeof window !== "undefined") {
+      const isSystemColorSchemeDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      const storageTheme = sessionStorage.getItem("theme");
+      if (!storageTheme && isSystemColorSchemeDark) {
+        document.querySelector('html').setAttribute('data-theme', "dark");
+      } else if (storageTheme === "dark") {
+        document.documentElement.classList.add("dark");
+        document.head.children.namedItem("theme-color").content = "#0f172a";
+      } else {
+        // we already server render light theme
+        document.head.children.namedItem("theme-color").content = "#ffffff";
+      }
+    }
+  })();
+</script>
+
