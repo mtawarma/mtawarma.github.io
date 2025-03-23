7cd7453e45fde1dab254f0127c9a4f15a9734b19
Tawarmalani, Mohit <mtawarma@purdue.edu>
2025-03-23 00:31:52 -0400

    move to tailwind v4 make adjustments for tailwind v4 fix for build for tailwind v4 add directories fix search visibility in dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import pagefind from "astro-pagefind";
-
 import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mohit.prof/',
   base: '/',
-  integrations: [svelte(), tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), pagefind()]
+  output: "static",
+  build: {
+    format: "directory",
+  },
+
+  vite: {
+    plugins: [tailwindcss(), pagefind()],
+  },
+
+  integrations: [mdx(), svelte(), sitemap()],
 });
